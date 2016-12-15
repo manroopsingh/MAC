957d215fa069c0f9a0ba27a1771d1cdecc3e93c1
manroopsingh <manroopmsingh@gmail.com>
2016-12-15 17:56:19 -0500

    semi update on populating listview

--- classpractise1/src/main/java/com/example/inspiron/classpractise1/SecondActivity.java
+++ classpractise1/src/main/java/com/example/inspiron/classpractise1/SecondActivity.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     private static final String TAG = "SecondActivityTAG_";
     TextView name;
+    ArrayList<User> users;
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +26,19 @@ public class SecondActivity extends AppCompatActivity {
 
 
         //getIntent and display from edittext from main activity
-        name = (TextView) findViewById(R.id.TextView_Name);
-        String myName = getIntent().getStringExtra("name");
-        name.setText(myName);
+//        name = (TextView) findViewById(R.id.TextView_Name);
+//        String myName = getIntent().getStringExtra("name");
+//        name.setText(myName);
+
+        users = new ArrayList<>();
+        users.add(new User("Manny", "Singh"));
+
+
+        ArrayAdapter<User> arrayAdapter = new ArrayAdapter<>(this, R.layout.simple_list_item1, users);
+        listView = (ListView) findViewById(R.id.lv_userlist);
+
+        listView.setAdapter(arrayAdapter);
+
 
         //parcelable
 //
