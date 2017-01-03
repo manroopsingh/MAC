caf7d6858667dffeb822f4ecd5a26f2baa351300
manroopsingh <manroopmsingh@gmail.com>
2017-01-03 15:01:25 -0500

    class practice 12 upload. Check readme for details

---
+++ classpractice12/README.md
@@ -0,0 +1,5 @@
+Class practice 12
+
+- Firebase connectivity(Manual and with Firebase assistant)
+- Firebase ui authentication library from github used instead of traditional login from Firebase library
+- Worked on Login using Email and Google Account
---
+++ classpractice12/src/main/java/com/example/inspiron/classpractice12/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.inspiron.classpractice12;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ui.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.tv_user);
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            textView.setText(auth.getCurrentUser().getDisplayName());
+            Toast.makeText(this, "Sign in", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "No Signed in", Toast.LENGTH_SHORT).show();
+            CallLogInScreen();
+        }
+    }
+
+    private void CallLogInScreen() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build(),
+                                new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()))
+                        .build(),
+                747);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow
+        if (requestCode == 747) {
+            if (resultCode == RESULT_OK) {
+                // user is signed in!
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                return;
+            }
+            // Sign in canceled
+            if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            // No network
+            if (resultCode == ResultCodes.RESULT_NO_NETWORK) {
+                Toast.makeText(this, "No internet", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // User is not signed in. Maybe just wait for the user to press
+            // "sign in" again, or show a message.
+        }
+    }
+
+    public void SignOut(View view) {
+        AuthUI.getInstance()
+                .signOut(this);
+        CallLogInScreen();
+    }
+
+}
