3d42817eca42b812debdadb242ce439593c2afea
manroopsingh <manroopmsingh@gmail.com>
2017-01-06 00:31:46 -0500

    classpractice15: recyclerview example. Populating the view complete. Adding, deleting pending. Orientation change on button click pending.

--- classpractice13/src/main/java/com/example/inspiron/classpractice13/MainActivity.java
+++ classpractice13/src/main/java/com/example/inspiron/classpractice13/MainActivity.java
@@ -2,18 +2,10 @@ package com.example.inspiron.classpractice13;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import com.example.inspiron.classpractice13.entities.Book;
 import com.orm.SugarContext;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,30 +36,32 @@ public class MainActivity extends AppCompatActivity {
         SugarContext.terminate();
     }
 
-    public void AddToList(View view) {
-        title = booktitle.getText().toString();
-        edition = bookedition.getText().toString();
+    //Commenting this code to ignore errors in other module gradle buidling
 
-        Book book = new Book(title, edition);
-        book.save();
-        Toast.makeText(this, "Added to List", Toast.LENGTH_SHORT).show();
-
-        booktitle.setText("");
-        bookedition.setText("");
-    }
-
-    public void ViewList(View view) {
-        List<String> bookslist = new ArrayList<>();
-        List<Book> books = new ArrayList<>();
-        books = Book.listAll(Book.class);
-        Log.d(TAG, "Sugar: " + books.size());
-        for(Book b: books ) {
-            Log.d(TAG, b.toString());
-            bookslist.add(b.toString());
-        }
-
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,bookslist);
-
-        booklist.setAdapter(adapter);
-    }
+//    public void AddToList(View view) {
+//        title = booktitle.getText().toString();
+//        edition = bookedition.getText().toString();
+//
+//        Book book = new Book(title, edition);
+//        book.save();
+//        Toast.makeText(this, "Added to List", Toast.LENGTH_SHORT).show();
+//
+//        booktitle.setText("");
+//        bookedition.setText("");
+//    }
+//
+//    public void ViewList(View view) {
+//        List<String> bookslist = new ArrayList<>();
+//        List<Book> books = new ArrayList<>();
+//        books = Book.listAll(Book.class);
+//        Log.d(TAG, "Sugar: " + books.size());
+//        for(Book b: books ) {
+//            Log.d(TAG, b.toString());
+//            bookslist.add(b.toString());
+//        }
+//
+//        ArrayAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,bookslist);
+//
+//        booklist.setAdapter(adapter);
+//    }
 }
---
+++ classpractice15/src/main/java/com/example/inspiron/classpractice15/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.inspiron.classpractice15;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Movie> movieList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private MoviesAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private EditText editText_title, editText_genre, editText_year;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+       // editText_title = (EditText) find
+
+        mAdapter = new MoviesAdapter(movieList);
+        mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        prepareMovieData();
+    }
+
+    private void prepareMovieData() {
+        Movie movie = new Movie("Mad Max: Fury Road", "Action & Adventure", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("Inside Out", "Animation, Kids & Family", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("Shaun the Sheep", "Animation", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("The Martian", "Science Fiction & Fantasy", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("Mission: Impossible Rogue Nation", "Action", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("Up", "Animation", "2009");
+        movieList.add(movie);
+
+        movie = new Movie("Star Trek", "Science Fiction", "2009");
+        movieList.add(movie);
+
+        movie = new Movie("The LEGO Movie", "Animation", "2014");
+        movieList.add(movie);
+
+        movie = new Movie("Iron Man", "Action & Adventure", "2008");
+        movieList.add(movie);
+
+        movie = new Movie("Aliens", "Science Fiction", "1986");
+        movieList.add(movie);
+
+        movie = new Movie("Chicken Run", "Animation", "2000");
+        movieList.add(movie);
+
+        movie = new Movie("Back to the Future", "Science Fiction", "1985");
+        movieList.add(movie);
+
+        movie = new Movie("Raiders of the Lost Ark", "Action & Adventure", "1981");
+        movieList.add(movie);
+
+        movie = new Movie("Goldfinger", "Action & Adventure", "1965");
+        movieList.add(movie);
+
+        movie = new Movie("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
+        movieList.add(movie);
+
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public void AddNewMovie(View view) {
+
+
+    }
+
+    public void recyclerVertical(View view) {
+    }
+
+    public void recyclerHorizontal(View view) {
+
+        mLayoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setLayoutManager(mLayoutManager);
+    }
+
+    public void recyclerGrid(View view) {
+    }
+}
